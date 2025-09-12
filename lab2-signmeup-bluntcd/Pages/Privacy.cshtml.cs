fd776d98acb0aec173fc9917e1f4cda045bf6a51
ichanblunt <ichanblunt@gmail.com>
2025-09-12 15:44:29 -0400

    Add project files.

---
+++ lab2-signmeup-bluntcd/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab2_signmeup_bluntcd.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ lab2-signmeup-bluntcd/Pages/signup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab2_signmeup_bluntcd.Pages
+{
+    public class signupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
